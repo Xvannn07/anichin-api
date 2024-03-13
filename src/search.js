b6a73314199309a77e253aa8728a47b8d7700e62
Xvannn 文 <95283773+Xvannn07@users.noreply.github.com>
2024-03-14 07:42:37 +0800

    Update search.js

--- src/search.js
+++ src/search.js
@@ -5,8 +5,7 @@ async function Getsearch(querys) {
     let res = await axios.request({
         method: "GET",
         url: "https://anichin.plus/?s=" + querys,
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+        headers {
             "Referer": "https://anichin.plus/"
         }
     });
